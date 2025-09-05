8aebf97268ea636e1867e571f06055ae2dd1c035
Armaan khan <khanedu101@gmail.com>
2025-09-05 09:56:51 +0530

    changs:

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -3,7 +3,13 @@ import express from "express";
 const app = express();
 
 app.post('/hook/catch/:userId/:zapId', (req, res) => {
+    
+    //path parameters not the query parametes 
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
 
+    // store in DB a new trigger
+    // push the event into a queue 
 })
 
 app.listen(3000)
