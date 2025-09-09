58db8286c4075eea9dfa17d12a4c2c21408f6196
Armaan khan <khanedu101@gmail.com>
2025-09-09 08:58:06 +0530

    changes

---
+++ processor/prisma/migrations/20250907175953_new/migration.sql
@@ -0,0 +1,98 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Zap" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Zap_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Trigger" (
+    "id" TEXT NOT NULL,
+    "AvailabletriggerId" TEXT NOT NULL,
+    "zapId" TEXT NOT NULL,
+
+    CONSTRAINT "Trigger_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Action" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "zapId" TEXT NOT NULL,
+    "availableActionid" TEXT NOT NULL,
+
+    CONSTRAINT "Action_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."AvailableActions" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "AvailableActions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."AvailableTriggers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "AvailableTriggers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ZapRun" (
+    "id" TEXT NOT NULL,
+    "zapId" TEXT NOT NULL,
+    "metadata" JSONB NOT NULL,
+
+    CONSTRAINT "ZapRun_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."zapRunOutbox" (
+    "id" TEXT NOT NULL,
+    "zapRunID" TEXT NOT NULL,
+
+    CONSTRAINT "zapRunOutbox_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Zap_id_key" ON "public"."Zap"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Trigger_zapId_key" ON "public"."Trigger"("zapId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Action_id_key" ON "public"."Action"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "zapRunOutbox_zapRunID_key" ON "public"."zapRunOutbox"("zapRunID");
+
+-- AddForeignKey
+ALTER TABLE "public"."Trigger" ADD CONSTRAINT "Trigger_AvailabletriggerId_fkey" FOREIGN KEY ("AvailabletriggerId") REFERENCES "public"."AvailableTriggers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Trigger" ADD CONSTRAINT "Trigger_zapId_fkey" FOREIGN KEY ("zapId") REFERENCES "public"."Zap"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Action" ADD CONSTRAINT "Action_zapId_fkey" FOREIGN KEY ("zapId") REFERENCES "public"."Zap"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Action" ADD CONSTRAINT "Action_availableActionid_fkey" FOREIGN KEY ("availableActionid") REFERENCES "public"."AvailableActions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ZapRun" ADD CONSTRAINT "ZapRun_zapId_fkey" FOREIGN KEY ("zapId") REFERENCES "public"."Zap"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."zapRunOutbox" ADD CONSTRAINT "zapRunOutbox_zapRunID_fkey" FOREIGN KEY ("zapRunID") REFERENCES "public"."ZapRun"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ processor/src/index.ts
@@ -0,0 +1,52 @@
+
+import { Kafka } from "kafkajs";
+import { PrismaClient } from "../generated/prisma"
+
+const client =  new PrismaClient()
+ 
+const TOPIC_NAME = "zap-events"
+
+const kafka = new Kafka ({
+    clientId : "outbox-processor",
+    brokers: ['localhost:9092']
+})
+interface pendingRows {
+    id : string;
+    zapRunID : string;
+}
+
+async function main (){
+    const producer =  kafka.producer();
+    await producer.connect();
+
+    while (1){
+
+        const pendingRows : pendingRows[] = await client.zapRunOutbox.findMany({
+            where : {},
+            take : 10
+        });
+  
+        producer.send({
+            topic : TOPIC_NAME,
+            // messages: pendingRows.map(r => ({
+            //     value : r.zapRunID
+            // }))
+            messages: pendingRows.map(r => {
+                return {
+                    value: r.zapRunID
+                }
+            })
+        })
+
+        // delete the zapoutbox entries from the database 
+        await client.zapRunOutbox.deleteMany({
+            where: {
+                id : {
+                    in : pendingRows.map(x => x.id) // delete an array of id's
+                }
+            }
+        })
+
+    }
+
+}
