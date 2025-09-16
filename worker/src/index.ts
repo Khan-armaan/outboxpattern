afb1caecbc7ed5a32a58dfb9d364b2e8a36ccbc9
Armaan khan <khanedu101@gmail.com>
2025-09-16 19:00:41 +0530

    chagnes

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,55 +1,214 @@
-// Pull from queue and do some work
-import { Kafka } from "kafkajs";
+// // Pull from queue and do some work
+// import { Kafka } from "kafkajs";
+// import { PrismaClient } from "../generated/prisma";
 
-const TOPIC_NAME = "zap-events";
+// const prisma = new PrismaClient()
+// const TOPIC_NAME = "zap-events";
+
+// const kafka = new Kafka({
+//     clientId: 'outbox-processor',
+//     brokers: ['localhost:9902']
+// })
+
+// async function main () {
+//     const consumer = kafka.consumer({
+//         groupId: 'main-worker'
+//     })
+//     await consumer.connect();
+
+//     await consumer.subscribe({
+//         topic : TOPIC_NAME,
+//         fromBeginning : true
+
+//     })
+
+//     await consumer.run({
+//         autoCommit: false,
+//         eachMessage : async ({ topic,partition, message}) => {
+//             console.log({
+//                 partition,
+//                 offset: message.offset,
+//                 value: message.value?.toString()
+//             })
+//         //    await new Promise((resolve) => setTimeout(resolve, 1000)) 
+//             if (!message.value) {
+//                 return;
+//             }
+
+//         const parsedValue = JSON.parse(message.value.toString())
+
+//         const zapRunID = parsedValue.zapRunID;
+//         const stage = parsedValue.stage;
+
+//         const zapRunDetails = prisma.zapRun.findFirst({
+//             where :{
+//                 id : zapRunID
+//             },
+//             include: {
+//                 Zap : {
+//                     include : {
+//                         actions : {
+//                             include :{
+//                                 type : true
+//                             }
+                          
+//                         }
+//                     }
+//                 } 
+//             }
+//         });
+//         //if you do not want to run this recursive query you can send 3 queries f
+//         // to find the zap id from the zaprun
+//         //then to find the associated actions of that zap
+//         // then to find the associated actions type for that actions  
+
+//         //find out what is the current aciton
+//         const currentAction = zapRunDetails?.Zap.actions.filter( x => x.sortingOrder === stage);
+
+//         if (!currentAction) {
+//             console.log(" no actions found")
+
+//         }
+//         // we will be executiuong only one action at a time only 
+//         if (currentAction.type.id === "email"){
+//             // parse out the email and the body to send 
+//         }
+
+//         if(currentAction.type.id === "send-sol"){
+//             // parse out the solana and the amount to send and the address 
+//         }
+
+//         const lastStage = (zapRunDetails.Zap.actions.length) - 1;
+//         if (stage !== lastStage) {
+
+//         }
+
+
+//            //give some sort of acknowlegdement to the kafka broker commit offset
+//            // topic is the queue name
+//            // offset is the message id
+//               await consumer.commitOffsets([
+//                 {
+//                  topic : TOPIC_NAME,
+//                  partition,
+//                  offset: (Number(message.offset) + 1).toString()
+//                 }
+//               ])
+//         }
+//     })
+
+// }
+// main()
+require('dotenv').config()
+
+import { PrismaClient } from "../generated/prisma";
+import { JsonObject } from "@prisma/client/runtime/library";
+import { Kafka } from "kafkajs";
+import { parse } from "./parser" ;
+import { sendEmail } from "./email";
+import { sendSol } from "./solana";
+
+const prismaClient = new PrismaClient();
+const TOPIC_NAME = "zap-events"
 
 const kafka = new Kafka({
-    clientId: 'outbox-processor',
-    brokers: ['localhost:9902']
+    clientId: 'outbox-processor-2',
+    brokers: ['localhost:9092']
 })
 
-async function main () {
-    const consumer = kafka.consumer({
-        groupId: 'main-worker'
-    })
+async function main() {
+    const consumer = kafka.consumer({ groupId: 'main-worker-2' });
     await consumer.connect();
+    const producer =  kafka.producer();
+    await producer.connect();
 
-    await consumer.subscribe({
-        topic : TOPIC_NAME,
-        fromBeginning : true
-
-    })
+    await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true })
 
     await consumer.run({
         autoCommit: false,
-        eachMessage : async ({ topic,partition, message}) => {
-            console.log({
-                partition,
-                offset: message.offset,
-                value: message.value?.toString()
-            })
-        //    await new Promise((resolve) => setTimeout(resolve, 1000)) 
-            if (!message.value) {
-                return;
-            }
+        eachMessage: async ({ topic, partition, message }) => {
+          console.log({
+            partition,
+            offset: message.offset,
+            value: message.value?.toString(),
+          })
+          if (!message.value?.toString()) {
+            return;
+          }
 
-        const parsedValue = JSON.parse(message.value.toString())
+          const parsedValue = JSON.parse(message.value?.toString());
+          const zapRunId = parsedValue.zapRunId;
+          const stage = parsedValue.stage;
 
-        const zapRunID = parsedValue.zapRunID;
-        const stage = parsedValue.stage;
-
-           //give some sort of acknowlegdement to the kafka broker commit offset
-           // topic is the queue name
-           // offset is the message id
-              await consumer.commitOffsets([
-                {
-                 topic : TOPIC_NAME,
-                 partition,
-                 offset: (Number(message.offset) + 1).toString()
+          const zapRunDetails = await prismaClient.zapRun.findFirst({
+            where: {
+              id: zapRunId
+            },
+            include: {
+              zap: {
+                include: {
+                  actions: {
+                    include: {
+                      type: true
+                    }
+                  }
                 }
-              ])
-        }
-    })
+              },
+            }
+          });
+          const currentAction = zapRunDetails?.zap.actions.find(x => x.sortingOrder === stage);
+
+          if (!currentAction) {
+            console.log("Current action not found?");
+            return;
+          }
+
+          const zapRunMetadata = zapRunDetails?.metadata;
+
+          if (currentAction.type.id === "email") {
+            const body = parse((currentAction.metadata as JsonObject)?.body as string, zapRunMetadata);
+            const to = parse((currentAction.metadata as JsonObject)?.email as string, zapRunMetadata);
+            console.log(`Sending out email to ${to} body is ${body}`)
+            await sendEmail(to, body);
+          }
+
+          if (currentAction.type.id === "send-sol") {
+
+            const amount = parse((currentAction.metadata as JsonObject)?.amount as string, zapRunMetadata);
+            const address = parse((currentAction.metadata as JsonObject)?.address as string, zapRunMetadata);
+            console.log(`Sending out SOL of ${amount} to address ${address}`);
+            await sendSol(address, amount);
+          }
+          
+          // 
+          await new Promise(r => setTimeout(r, 500));
+
+          const lastStage = (zapRunDetails?.zap.actions?.length || 1) - 1; // 1
+          console.log(lastStage);
+          console.log(stage);
+          if (lastStage !== stage) {
+            console.log("pushing back to the queue")
+            await producer.send({
+              topic: TOPIC_NAME,
+              messages: [{
+                value: JSON.stringify({
+                  stage: stage + 1,
+                  zapRunId
+                })
+              }]
+            })  
+          }
+
+          console.log("processing done");
+          // 
+          await consumer.commitOffsets([{
+            topic: TOPIC_NAME,
+            partition: partition,
+            offset: (parseInt(message.offset) + 1).toString() // 5
+          }])
+        },
+      })
 
 }
+
 main()
